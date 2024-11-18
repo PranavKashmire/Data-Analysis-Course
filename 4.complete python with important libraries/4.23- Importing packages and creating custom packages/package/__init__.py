0546f2ac7071025bbd80e22b869a6c2cd3fe90a0
Pranav Kashmire <pranav.kashmire09@gmail.com>
2024-11-18 15:32:49 +0530

    Add files via upload

---
+++ 4.complete python with important libraries/4.23- Importing packages and creating custom packages/package/__init__.py
@@ -0,0 +1,7 @@
+#this is an init file which is very important and special file  
+
+#this file is empty and it is used to tell python that this is a custom package and 
+# it isitializes our custom functions which we create inside this package
+
+
+#now lets create our custom package "abcd.py" and function "pqr" inside it
---
+++ 4.complete python with important libraries/4.23- Importing packages and creating custom packages/package/abcd.py
@@ -0,0 +1,6 @@
+# abcd.py is our custom package and inside it we have created a custom function "pqr"
+def pqr(a,b):
+   print(a+b)
+
+
+   #here we can create any number of functions as per our requirement and use them 
